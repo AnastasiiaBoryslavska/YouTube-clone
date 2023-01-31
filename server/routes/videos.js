a9422847ac7432e8315564895ae787c008657f7c
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2023-01-30 22:20:35 -0500

    creating routes for add, update, delete and get a video

---
+++ server/routes/videos.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  addVideo,
+  deleteVideo,
+  getVideo,
+  updateVideo,
+} from "../controllers/video.js";
+import { verifyToken } from "../verifyToken.js";
+
+const router = express.Router();
+
+router.post("/", verifyToken, addVideo);
+router.put("/:id", verifyToken, updateVideo);
+router.delete("/:id", verifyToken, deleteVideo);
+router.get("/find/:id", getVideo);
+
+export default router;
